a622e14a8d3905ffe3e1df4e9704a45736fc46e9
Max Kamerov <indigowar.github@gmail.com>
2024-04-17 18:48:24 +0300

    added storage interface for accounts

---
+++ services/accounts/internal/service/storage.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+
+	"github.com/indigowar/food_out/services/accounts/internal/domain"
+)
+
+var (
+	ErrAccountNotFoundInStorage = errors.New("account was not found in the storage")
+	ErrAccountAlreadyInStorage  = errors.New("account already exists in the storage")
+)
+
+//go:generate moq -out storage_moq_test.go . Storage
+
+type Storage interface {
+	GetAll(ctx context.Context) ([]*domain.Account, error)
+	GetByID(ctx context.Context, id uuid.UUID) (*domain.Account, error)
+	GetByPhone(ctx context.Context, phone string) (*domain.Account, error)
+	Add(ctx context.Context, account *domain.Account) error
+	Remove(ctx context.Context, id uuid.UUID) error
+	Update(ctx context.Context, account *domain.Account) error
+}
