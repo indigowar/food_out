1bed4c33b532343d0cb82172d367d1724e97847d
Max Kamerov <indigowar.github@gmail.com>
2024-04-19 15:34:59 +0300

    made some changes to accounts service

--- services/accounts/internal/service/service.go
+++ services/accounts/internal/service/service.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"errors"
+	"log/slog"
 	"net/url"
 
 	"github.com/google/uuid"
@@ -9,8 +11,17 @@ import (
 	"github.com/indigowar/food_out/services/accounts/internal/domain"
 )
 
+var (
+	ErrNotFound                = errors.New("account is not found")
+	ErrPhoneNumberAlreadyInUse = errors.New("phone number is already in use")
+	ErrInvalidCredentials      = errors.New("invalid credentials")
+	ErrInvalidValue            = errors.New("provided value is invalid")
+	ErrInternal                = errors.New("internal service error")
+)
+
 type Service struct {
 	storage Storage
+	logger  *slog.Logger
 }
 
 func (svc *Service) GetAccountByID(ctx context.Context, id uuid.UUID) (*domain.Account, error) {
@@ -41,8 +52,30 @@ func (svc *Service) UpdateProfilePicture(ctx context.Context, id uuid.UUID, prof
 	panic("not implemented")
 }
 
-func NewService(storage Storage) *Service {
+func (svc *Service) GetUserIDByCredentials(ctx context.Context, phone string, password string) (uuid.UUID, error) {
+	// account, err := svc.storage.GetByPhone(ctx, phone)
+	// if err != nil {
+	// 	if errors.Is(err, ErrAccountNotFoundInStorage) {
+	// 		svc.logger.Info("Searched user account by phone does not exists")
+	// 		return uuid.UUID{}, ErrNotFound
+	// 	}
+	// 	svc.logger.Warn("Failed to search user account by id")
+	//
+	// 	return uuid.UUID{}, ErrInternal
+	// }
+	//
+	// if account.Password() != password {
+	// 	svc.logger.Info("Password check failed, while attempting to access user id by credentials")
+	// 	return uuid.UUID{}, ErrNotFound
+	// }
+	//
+	// return account.ID(), nil
+	panic("not implemented")
+}
+
+func NewService(storage Storage, logger *slog.Logger) *Service {
 	return &Service{
 		storage: storage,
+		logger:  logger,
 	}
 }
