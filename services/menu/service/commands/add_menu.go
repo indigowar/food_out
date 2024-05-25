cc2d58ca296b27955970d2df23a66888c6e5c352
indigowar <a10levsion@gmail.com>
2024-05-25 23:46:06 +0300

    implemented AddMenuCommand

--- services/menu/service/commands/add_menu.go
+++ services/menu/service/commands/add_menu.go
@@ -2,10 +2,14 @@ package commands
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/url"
 
 	"github.com/google/uuid"
+
+	"github.com/indigowar/food_out/services/menu/domain"
 )
 
 type AddMenuArgument struct {
@@ -22,7 +26,42 @@ type AddMenuCommand struct {
 }
 
 func (cmd *AddMenuCommand) AddMenu(ctx context.Context, args AddMenuArgument) (uuid.UUID, error) {
-	panic("not implemented")
+	menu, err := domain.NewMenu(args.Name, args.Restaurant, args.Image)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("%w: %s", ErrInvalidData, err)
+	}
+
+	empty := uuid.UUID{}
+	if args.Restaurant == empty {
+		return uuid.UUID{}, ErrInvalidData
+	}
+
+	if exists, err := cmd.restaurants.RestaurantExists(ctx, args.Restaurant); !exists || err != nil {
+		if !exists {
+			return uuid.UUID{}, ErrRestaurantNotFound
+		}
+		cmd.logInternalError("RestaurantExists", "RestaurantStoragePort", err)
+		return uuid.UUID{}, err
+	}
+
+	if err := cmd.menus.AddMenu(ctx, menu); err != nil {
+		if errors.Is(err, ErrAlreadyExists) {
+			return uuid.UUID{}, ErrMenuAlreadyExists
+		}
+		cmd.logInternalError("AddMenu", "MenuStoragePort", err)
+		return uuid.UUID{}, err
+	}
+
+	return menu.ID(), nil
+}
+
+func (cmd *AddMenuCommand) logInternalError(action string, reason string, err error) {
+	cmd.logger.Warn(
+		"AddMenu Command FAILED",
+		"action", action,
+		"reason", reason,
+		"error", err.Error(),
+	)
 }
 
 func NewAddMenuCommand(
