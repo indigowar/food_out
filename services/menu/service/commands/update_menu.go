86372e1cbcde88638b52b2068accc1522f68fda9
indigowar <a10levsion@gmail.com>
2024-05-27 17:14:08 +0300

    Added UpdateMenuCommand

--- services/menu/service/commands/update_menu.go
+++ services/menu/service/commands/update_menu.go
@@ -2,23 +2,98 @@ package commands
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log/slog"
 	"net/url"
 
 	"github.com/google/uuid"
+
+	"github.com/indigowar/food_out/services/menu/domain"
 )
 
 type UpdateMenuArgument struct {
-	ID    uuid.UUID
-	Name  string
-	Image *url.URL
+	ID         uuid.UUID
+	Restaurant uuid.UUID
+	Name       string
+	Image      *url.URL
 }
 
-type UpdateMenuCommand struct{}
+type UpdateMenuCommand struct {
+	restaurant RestaurantStoragePort
+	menu       MenuStoragePort
+
+	logger *slog.Logger
+}
 
 func (cmd *UpdateMenuCommand) UpdateMenu(ctx context.Context, arg UpdateMenuArgument) error {
-	panic("not implemented")
+	if exists, err := cmd.restaurant.RestaurantExists(ctx, arg.Restaurant); !exists || err != nil {
+		if err != nil {
+			return err
+		}
+		return ErrRestaurantNotFound
+	}
+
+	menu, err := cmd.menu.GetMenu(ctx, arg.ID)
+	if err != nil {
+		if errors.Is(err, ErrNotFound) {
+			return ErrMenuNotFound
+		}
+		cmd.logInternalError("GetMenu", "MenuStoragePort", err)
+		return ErrInternal
+	}
+
+	if menu.Restaurant() != arg.Restaurant {
+		return ErrMenuNotFound
+	}
+
+	if err := cmd.updateMenu(menu, arg); err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidData, err.Error())
+	}
+
+	return cmd.saveMenu(ctx, menu)
 }
 
-func NewUpdateMenuCommand() *UpdateMenuCommand {
-	panic("not implemented")
+func (cmd *UpdateMenuCommand) updateMenu(menu *domain.Menu, arg UpdateMenuArgument) error {
+	if err := menu.SetName(arg.Name); err != nil {
+		return err
+	}
+
+	if err := menu.SetImage(arg.Image); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cmd *UpdateMenuCommand) saveMenu(ctx context.Context, menu *domain.Menu) error {
+	if err := cmd.menu.UpdateMenu(ctx, menu); err != nil {
+		if errors.Is(err, ErrNotFound) {
+			return ErrMenuNotFound
+		}
+		cmd.logInternalError("GetMenu", "MenuStoragePort", err)
+		return ErrInternal
+	}
+	return nil
+}
+
+func (cmd *UpdateMenuCommand) logInternalError(action string, reason string, err error) {
+	cmd.logger.Warn(
+		"UpdateDish Command FAILED",
+		"action", action,
+		"reason", reason,
+		"error", err.Error(),
+	)
+}
+
+func NewUpdateMenuCommand(
+	restaurant RestaurantStoragePort,
+	menu MenuStoragePort,
+	logger *slog.Logger,
+) *UpdateMenuCommand {
+	return &UpdateMenuCommand{
+		restaurant: restaurant,
+		menu:       menu,
+		logger:     logger,
+	}
 }
