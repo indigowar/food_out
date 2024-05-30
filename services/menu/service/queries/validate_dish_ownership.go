8b44012d89876363aabd81f3866cea1c5f2632fc
indigowar <a10levsion@gmail.com>
2024-05-30 14:39:16 +0300

    Implemented ValidateDishOwnershipQuery

--- services/menu/service/queries/validate_dish_ownership.go
+++ services/menu/service/queries/validate_dish_ownership.go
@@ -2,16 +2,74 @@ package queries
 
 import (
 	"context"
+	"errors"
+	"log/slog"
 
 	"github.com/google/uuid"
 )
 
-type ValidateDishOwnershipQuery struct{}
+type ValidateDishOwnershipQuery struct {
+	menus      MenuRetriever
+	restaurant RestaurantRetriever
 
-func (q *ValidateDishOwnershipQuery) ValidateDishOwnership(ctx context.Context, restaurantId uuid.UUID) (bool, error) {
-	panic("not implemented")
+	logger *slog.Logger
 }
 
-func NewValidateDishOwnershipQuery() *ValidateDishOwnershipQuery {
-	panic("not implemented")
+func (q *ValidateDishOwnershipQuery) ValidateDishOwnership(ctx context.Context, restaurantId uuid.UUID, dishesId []uuid.UUID) (bool, error) {
+	if exists, err := q.restaurant.RestaurantExists(ctx, restaurantId); !exists || err != nil {
+		if err != nil {
+			q.logInternalError("RestaurantExists", "RestaurantRetriever", err)
+			return false, ErrInternal
+		}
+		return false, ErrRestaurantNotFound
+	}
+
+	menus, err := q.menus.RetrieveByRestaurant(ctx, restaurantId)
+	if err != nil {
+		if errors.Is(err, ErrNotFound) {
+			return false, ErrRestaurantNotFound
+		}
+		q.logInternalError("RetrieveByRestaurant", "MenuRetriever", err)
+		return false, ErrInternal
+	}
+
+	dishPresence := make(map[uuid.UUID]bool)
+
+	for _, dish := range dishesId {
+		dishPresence[dish] = false
+		for _, menu := range menus {
+			if menu.HasDish(dish) {
+				dishPresence[dish] = true
+			}
+		}
+	}
+
+	for _, precense := range dishPresence {
+		if !precense {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+func (q *ValidateDishOwnershipQuery) logInternalError(action string, reason string, err error) {
+	q.logger.Warn(
+		"ValidateDishOwnership Query FAILED",
+		"action", action,
+		"reason", reason,
+		"error", err.Error(),
+	)
+}
+
+func NewValidateDishOwnershipQuery(
+	menus MenuRetriever,
+	restaurant RestaurantRetriever,
+	logger *slog.Logger,
+) *ValidateDishOwnershipQuery {
+	return &ValidateDishOwnershipQuery{
+		menus:      menus,
+		restaurant: restaurant,
+		logger:     logger,
+	}
 }
