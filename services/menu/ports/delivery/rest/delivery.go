7972fbe2621016a2cdf3ec050652121f147c63a9
Indigowar <a10levsion@gmail.com>
2024-07-05 18:35:17 +0300

    Added menu REST specification.
    Added REST specification for Menu service and generated boilerplate
    code.
    Made a simple Delivery struct for wrapping REST code over service.

---
+++ services/menu/ports/delivery/rest/delivery.go
@@ -0,0 +1,64 @@
+package rest
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/indigowar/food_out/services/menu/ports/delivery/rest/api"
+)
+
+type Delivery struct {
+	logger *slog.Logger
+
+	server *http.Server
+}
+
+func (d *Delivery) Run(ctx context.Context) error {
+	errChan := make(chan error, 1)
+
+	go func() {
+		err := d.server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			d.logger.Warn("Server.ListenAndServe failed", "err", err)
+			errChan <- err
+		}
+	}()
+
+	d.logger.Info("Rest Server is started")
+
+	select {
+	case err := <-errChan:
+		return fmt.Errorf("Delivery error: Listening: %w", err)
+	case <-ctx.Done():
+		stopCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := d.server.Shutdown(stopCtx); err != nil {
+			d.logger.Warn("Server.Shutdown failed, forcing the shutdown", "err", err)
+			return fmt.Errorf("Delivery error: Shutdown: %w", err)
+		}
+		return nil
+	}
+}
+
+func New(
+	logger *slog.Logger,
+) (*Delivery, error) {
+	api, err := api.NewServer(&handler{})
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create API handler: %w", err)
+	}
+
+	server := &http.Server{
+		Addr:    ":80",
+		Handler: api,
+	}
+
+	return &Delivery{
+		logger: logger,
+		server: server,
+	}, nil
+}
---
+++ services/menu/ports/delivery/rest/handler.go
@@ -0,0 +1,37 @@
+package rest
+
+import (
+	"context"
+
+	"github.com/indigowar/food_out/services/menu/ports/delivery/rest/api"
+)
+
+type handler struct {
+}
+
+var _ api.Handler = &handler{}
+
+// RetrieveDishByID implements api.Handler.
+func (h *handler) RetrieveDishByID(ctx context.Context, params api.RetrieveDishByIDParams) (api.RetrieveDishByIDRes, error) {
+	panic("unimplemented")
+}
+
+// RetrieveDishesByMenuId implements api.Handler.
+func (h *handler) RetrieveDishesByMenuId(ctx context.Context, params api.RetrieveDishesByMenuIdParams) (api.RetrieveDishesByMenuIdRes, error) {
+	panic("unimplemented")
+}
+
+// RetrieveListOfRestaurants implements api.Handler.
+func (h *handler) RetrieveListOfRestaurants(ctx context.Context) (api.RetrieveListOfRestaurantsRes, error) {
+	panic("unimplemented")
+}
+
+// RetrieveMenuByID implements api.Handler.
+func (h *handler) RetrieveMenuByID(ctx context.Context, params api.RetrieveMenuByIDParams) (api.RetrieveMenuByIDRes, error) {
+	panic("unimplemented")
+}
+
+// RetrieveMenusByRestaurant implements api.Handler.
+func (h *handler) RetrieveMenusByRestaurant(ctx context.Context, params api.RetrieveMenusByRestaurantParams) (api.RetrieveMenusByRestaurantRes, error) {
+	panic("unimplemented")
+}
