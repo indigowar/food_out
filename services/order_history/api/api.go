1a79703a3c5f94dcfd37a64633d519e5261cbb12
indigowar <a10levsion@gmail.com>
2024-10-03 14:33:14 +0300

    feat: made empty API server

--- services/order_history/api/api.go
+++ services/order_history/api/api.go
@@ -1,15 +1,49 @@
 package api
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
-type API struct{}
+type API struct {
+	server *http.Server
+}
 
 func (api *API) Run(ctx context.Context) error {
-	// TODO: Implement API.Run
-	panic("unimplemented")
+	go func() {
+		if err := api.server.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := api.server.Shutdown(shutdownCtx); err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("failed to shut down the server: %w", err)
+	}
+
+	return nil
 }
 
 func NewAPI() *API {
-	// TODO: Implement NewAPI.
-	panic("unimplemented")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	server := &http.Server{
+		Addr:    ":80",
+		Handler: mux,
+	}
+
+	return &API{
+		server: server,
+	}
 }
