3e26e4b69f1846f34ff361c1e3be0e69122f3a74
indigowar <a10levsion@gmail.com>
2024-10-03 12:49:24 +0300

    feat: implemented Connect for postgres storage in Order History service

--- services/order_history/storage/postgres/connect.go
+++ services/order_history/storage/postgres/connect.go
@@ -1,15 +1,24 @@
 package postgres
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/jackc/pgx/v5"
 
 	"github.com/indigowar/food_out/services/order_history/config"
 )
 
-// TODO: Implement Connect function.
-
 func Connect(
 	config *config.Postgres,
 ) (*pgx.Conn, error) {
-	panic("unimplemented")
+	// "postgres://username:password@localhost:5432/database_name"
+	databaseUrl := fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s",
+		config.User, config.Password,
+		config.Host, config.Port,
+		config.Database,
+	)
+
+	return pgx.Connect(context.Background(), databaseUrl)
 }
